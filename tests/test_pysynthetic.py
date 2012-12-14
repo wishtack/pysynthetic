ae53aab03a784e6716a5775b266e75f18e71435a
Younes JAAIDI <yjaaidi@gmail.com>
2012-12-14 18:25:24 +0100

    MemberSynthesizer is almost ready. Still needs some tweaks for the init part.

---
+++ tests/test_pysynthetic.py
@@ -0,0 +1,51 @@
+
+import unittest
+
+from pysynthetic import synthesizeMember, AccessorNameMakerUnderscore
+
+@synthesizeMember('minimalistMember')
+@synthesizeMember('memberWithDefaultValue', defaultValue = "default")
+@synthesizeMember('customMember',
+            getterName = 'giveMeTheCustomMember',
+            setterName = 'giveThisToTheCustumMember',
+            privateMemberName = '_internalPrivateSecretMemberThatShouldNeverBeUsedOutsideThisClass')
+class TestBasic:
+    pass
+
+@synthesizeMember('member', accessorNameMaker = AccessorNameMakerUnderscore())
+class TestUnderscore:
+    pass
+
+class TestSynthesizer(unittest.TestCase):
+
+    def testSynthesize(self):
+        instance = TestBasic()
+        
+        # Default default ;) member value is None.
+        self.assertEqual(None, instance.minimalistMember())
+        
+        # Default set and get test. 
+        instance.setMinimalistMember(10)
+        self.assertEqual(10, instance.minimalistMember())
+        
+        # Checking custom default value.
+        self.assertEqual("default", instance.memberWithDefaultValue())
+        
+        # With custom accessor names and constructor value, default names don't work...
+        self.assertFalse(hasattr(instance, 'customMember'))
+        self.assertFalse(hasattr(instance, 'setCustomMember'))
+        self.assertFalse(hasattr(instance, '_customMember'))
+
+        # ... but custom names work.
+        instance.giveThisToTheCustumMember("newValue")
+        self.assertEqual("newValue", instance._internalPrivateSecretMemberThatShouldNeverBeUsedOutsideThisClass)
+        self.assertEqual("newValue", instance.giveMeTheCustomMember())
+    
+    def testSynthesizeWithAccessorNameMakerUnderscore(self):
+        pass
+    
+    def testContract(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
