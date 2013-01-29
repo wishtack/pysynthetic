2914b5aa5f4429aca9d3847e3452660ed58a2acd
Younes JAAIDI <yjaaidi@gmail.com>
2013-01-29 20:54:22 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (2, 7):
+    raise Exception('Pysynthetic requires Python 2.7 or higher.')
+
+# Dependencies
+install_requires = ['pycontracts >= 1.4.0']
+
+setup(
+    name="pysynthetic",
+    version="0.1",
+    platforms=['any'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=install_requires,
+    zip_safe=True)
+
