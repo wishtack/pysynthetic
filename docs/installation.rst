54b060e8fc72bb12dfc7ae15aa8c6f61ad0fa365
Younes JAAIDI <yjaaidi@gmail.com>
2013-03-23 20:27:41 +0100

    Added 'setup.py test' support. Removed 'PyParsing' version check hack as it was moved to 'PyContracts' thanks to Andrea CENSI.

--- docs/installation.rst
+++ docs/installation.rst
@@ -1,11 +1,9 @@
 Installation
 ************
 
-As there is a temporary issue with the **PySynthetic** => **PyContracts** => **PyParsing** dependency chain (**PyParsing** >= 2.0.0 is not compatible with Python 2), **PySynthetic** must be installed this way.
-
 .. code-block:: shell
 
-    pip install 'pyparsing<2.0.0' pysynthetic
+    pip install pysynthetic
 
 Or simply from the tarball or source code if you are not using *pip*.
 
--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-# @hack: PyContracts dependency PyParsing needs version 2.0.0 to work with Python 3 but this version doesn't work with Python 2.
-import sys
-pyparsing_version_condition = '<'
-if sys.version_info[0] >= 3:
-    pyparsing_version_condition = '>='
-
-setup(setup_requires=['pyparsing%s2.0.0' % pyparsing_version_condition,
-                      'd2to1'],
+setup(setup_requires=['d2to1'],
+      tests_require = ['mock', 'nose'],
+      test_suite = 'nose.collector',
       d2to1=True)
